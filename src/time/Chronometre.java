8cca1bf681b6a34893d541ffc2cc2ca17f383698
FaroukS <farouk@juniorucp.com>
2014-12-21 21:18:36 +0100

    modification du chrono

--- src/time/Chronometre.java
+++ src/time/Chronometre.java
@@ -1,12 +1,10 @@
 package time;
 
 public class Chronometre {
-	private Timer hours = new Timer(0, ChronometreParameter.MAX_HOURS, 0);
 	private Timer minutes = new Timer(0, ChronometreParameter.MAX_MINUTES, 0);
 	private Timer seconds = new Timer(0, ChronometreParameter.MAX_SECONDS, 0);
 
 	public Chronometre(int hours,int minutes, int seconds) {
-		this.hours = new Timer(hours, ChronometreParameter.MAX_HOURS, 0);
 		this.minutes = new Timer(minutes, ChronometreParameter.MAX_MINUTES, 0);
 		this.seconds = new Timer(seconds, ChronometreParameter.MAX_SECONDS, 0);
 	}
@@ -15,9 +13,6 @@ public class Chronometre {
 		seconds.increment();
 		if (seconds.getValue() == ChronometreParameter.MAX_SECONDS) {
 			minutes.increment();
-			if (minutes.getValue() == ChronometreParameter.MAX_MINUTES) {
-				hours.increment();
-			}
 		} 
 	}
 	
@@ -25,9 +20,6 @@ public class Chronometre {
 		seconds.decrement();
 		if (seconds.getValue() == ChronometreParameter.MAX_SECONDS) {
 			minutes.decrement();
-			if (minutes.getValue() == ChronometreParameter.MAX_MINUTES) {
-				hours.decrement();
-			}
 		}
 	}
 
@@ -39,18 +31,15 @@ public class Chronometre {
 		return minutes;
 	}
 
-	public Timer getHours() {
-		return hours;
-	}
 
 	public Boolean isEmpty() {
-		if (hours.isEmpty() && minutes.isEmpty() && seconds.isEmpty()) {
+		if (minutes.isEmpty() && seconds.isEmpty()) {
 			return true;
 		}
 		return false;
 	}
 
 	public String toString() {
-		return hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
+		return minutes.toString() + ":" + seconds.toString();
 	}
 }
